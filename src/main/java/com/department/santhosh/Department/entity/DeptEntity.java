a4e2eb864a37394e1e6e222be5c0bfc3de2ca603
kishorkumar13 <kishorreddy841@gmail.com>
2023-12-09 18:17:10 +0530

    done today

--- src/main/java/com/department/santhosh/Department/entity/DeptEntity.java
+++ src/main/java/com/department/santhosh/Department/entity/DeptEntity.java
@@ -9,46 +9,54 @@ import jakarta.persistence.Id;
 public class DeptEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	   private int deptId;
-	   private String  deptName;
-	   private String deptLoc;
-	   private String deptPhoneNo;
-	   
-	   
+	private int deptId;
+	private String deptName;
+	private String deptLoc;
+	private String deptPhoneNo;
+
 	public DeptEntity() {
-	
-		
+
 	}
+
 	public DeptEntity(int deptId, String deptName, String deptLoc, String deptPhoneNo) {
 		this.deptId = deptId;
 		this.deptName = deptName;
 		this.deptLoc = deptLoc;
 		this.deptPhoneNo = deptPhoneNo;
 	}
+
 	public int getDeptId() {
 		return deptId;
 	}
+
 	public void setDeptId(int deptId) {
 		this.deptId = deptId;
 	}
+
 	public String getDeptName() {
 		return deptName;
 	}
+
 	public void setDeptName(String deptName) {
 		this.deptName = deptName;
 	}
+
 	public String getDeptLoc() {
 		return deptLoc;
 	}
+
 	public void setDeptLoc(String deptLoc) {
 		this.deptLoc = deptLoc;
 	}
+
 	public String getDeptPhoneNo() {
 		return deptPhoneNo;
 	}
+
 	public void setDeptPhoneNo(String deptPhoneNo) {
 		this.deptPhoneNo = deptPhoneNo;
 	}
+
 	@Override
 	public String toString() {
 		return "DeptEntity [deptId=" + deptId + ", deptName=" + deptName + ", deptLoc=" + deptLoc + ", deptPhoneNo="
